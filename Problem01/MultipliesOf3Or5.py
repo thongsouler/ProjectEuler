bceea1bfe335199a69d78813b31de1317a40012f
Hoàng Thông <46652538+thongsouler@users.noreply.github.com>
2020-08-11 14:49:04 +0700

    Update MultipliesOf3Or5.py

--- Problem01/MultipliesOf3Or5.py
+++ Problem01/MultipliesOf3Or5.py
@@ -1,6 +1,6 @@
 #Note: List comprehension
-def sum_of_multiplies_3_or_5(to):
-    return sum([i for i in range (1, to) if i % 3 == 0 or i% 5 == 0])
 
+def sum_of_multiplies_3_or_5(to = 10):
+    return sum([i for i in range (1, to) if i % 3 == 0 or i % 5 == 0])
 if __name__ == "__main__":
     print(sum_of_multiplies_3_or_5(1000))
